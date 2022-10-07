81f5277cba0e477e1bc501d5d0ebffb0c370fb1e
ricardobossan <ricardobossan@protonmail.com>
2022-10-06 23:50:09 -0300

    Adiciona comentários ao código

--- c_program.c
+++ c_program.c
@@ -1,11 +1,15 @@
+// Inclui bibliotecas
 #include <math.h>
 #include <stdio.h>
+
 int main() {
+  // Meus dados
   printf("Nome: Ricardo Matos de Azevedo Bossan\n");
   printf("Curso: Análise e Desenvolvimento de Sistemas\n");
   printf("Universidade: Unilasalle\n");
   printf("matrícula: 202210731\n");
 
+  // Oferece ao usuário opções de operações
   int opName = 0;
   while (opName <= 0 || opName > 6) {
     printf("Qual operação deseja realizar (digite o número correspondente da "
@@ -14,8 +18,10 @@ int main() {
     scanf("%d", &opName);
   }
 
+  // Pede ao usuário que defina operandos
   float op1, op2;
   int eqZero = 1;
+  // Checa se usuário vai dividir por 0
   if (opName == 3) {
     while (eqZero == 1) {
       printf("Digite os operandos 1 e 2, separados por espaço:\n");
@@ -27,9 +33,11 @@ int main() {
         eqZero = 0;
       }
     }
+    // Se não for  raiz quadrada, há 2 operandos
   } else if (opName != 5) {
     printf("Digite os operandos 1 e 2, separados por espaço:\n");
     scanf("%f %f", &op1, &op2);
+    // Se for raiz quadrada, há apenas 1 operando
   } else {
     printf("Digite o operando:\n");
     scanf("%f", &op1);
@@ -37,6 +45,7 @@ int main() {
 
   float resultado;
 
+  // Executa operações
   switch (opName) {
   case 1:
     resultado = op1 + op2;
@@ -58,5 +67,6 @@ int main() {
     break;
   }
 
+  // Exibe resultado ao usuário
   printf("%.2f", resultado);
 }
